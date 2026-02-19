31e7f32917f5659a79eb5857a5d57bf38bcb93ec
kheder99 <khederibrahem99@gmail.com>
2026-02-19 16:02:49 +0300

    Final Project

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-const bcrypt = require('bcrypt');
-var mongoose = require('mongoose');
-var apiRouter = require('./routers/api');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+const bcrypt = require("bcrypt");
+var mongoose = require("mongoose");
+var apiRouter = require("./routers/api");
 // const  dbURI = "mongodb+srv://zain:cmX84A6ragK4h0XI@cluster0.yoddb.mongodb.net/hotels?retryWrites=true&w=majority";
-const dbURI = "mongodb://localhost:27017/hotels";
+const dbURI =
+  "mongodb+srv://khederibrahem99:0SGZwIPqRJEL9Q2C@cluster0.l7jdw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const dbURI = "mongodb://localhost:27017/hotels";
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const dbConn = mongoose.connection;
 
-dbConn.on("error", (err) => { console.error(err) });
-dbConn.once("open", () => { console.log("DB started successfully") });
-
+dbConn.on("error", (err) => {
+  console.error(err);
+});
+dbConn.once("open", () => {
+  console.log("DB started successfully");
+});
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 // app.use(passport.initialize());
 // app.use(passport.session());
 
+app.use("/api", apiRouter);
 
-app.use('/api', apiRouter);
-
-
-
-app.listen(process.env.PORT || '3000', console.log("listining to port 3000"));
+app.listen(process.env.PORT || "3000", console.log("listining to port 3000"));
