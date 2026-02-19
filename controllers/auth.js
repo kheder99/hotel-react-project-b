4f6541eb9265677c1350e8a8bf94efe74ef412a0
kheder99 <khederibrahem99@gmail.com>
2026-02-19 18:39:24 +0300

    Final Project

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,35 @@
-var jwt = require('jsonwebtoken');
-const dotenv = require('dotenv');
-const db = require('./db');
+var jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+const db = require("./db");
 dotenv.config();
 
 // access config var
 process.env.TOKEN_SECRET;
 
 const generateAccessToken = function (username, password) {
-<<<<<<< HEAD
-  return jwt.sign({ email: username, password: password }, process.env.TOKEN_SECRET, { expiresIn: '1d' });
-=======
-  return jwt.sign({ email: username, password: password }, process.env.TOKEN_SECRET, { expiresIn: '30d' });
->>>>>>> 7292a7bb72881106dacfc1257cc28c07b9bb546c
-}
+  return jwt.sign(
+    { email: username, password: password },
+    process.env.TOKEN_SECRET,
+    { expiresIn: "30d" },
+  );
+};
 const authenticateToken = async (req, res, next) => {
   console.log(req.headers);
-  const token = req.headers['accesstoken'];
+  const token = req.headers["accesstoken"];
   console.log(token);
   if (token == null) return res.sendStatus(401);
   jwt.verify(token, process.env.TOKEN_SECRET, async (err, user) => {
     console.log(err);
 
     if (err) return res.sendStatus(403);
-    var userr = await db.users.findOne({ 'email': user.email });
+    var userr = await db.users.findOne({ email: user.email });
     console.log(userr);
     req.user = userr;
     next();
   });
-}
-
-
+};
 
 module.exports = {
   generateAccessToken,
   authenticateToken,
-}
-
+};
--- controllers/db.js
+++ controllers/db.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
 const user = new schema({
@@ -7,145 +7,95 @@ const user = new schema({
     required: true,
     unique: true,
     minlength: 5,
-    maxlength: 255
+    maxlength: 255,
   },
   email: { type: String, required: true },
   role: {
     type: String,
     required: true,
-    enum: ['Admin', 'Hotel', 'User']
+    enum: ["Admin", "Hotel", "User"],
   },
   password: {
     type: String,
     required: true,
     minlength: 5,
-    maxlength: 255
+    maxlength: 255,
   },
   image: String,
 });
-
-<<<<<<< HEAD
-const categories = new schema({
-  name: { type: String, unique: true, required: true },
-  image: String,
-});
-=======
-
->>>>>>> 7292a7bb72881106dacfc1257cc28c07b9bb546c
 
 const hotels = new schema({
   user: {
     type: schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   name: { type: String, unique: true, required: true },
   images: [String],
   description: String,
-<<<<<<< HEAD
-  category: {
-    type: schema.Types.ObjectId,
-    ref: "Categories"
-  },
-=======
->>>>>>> 7292a7bb72881106dacfc1257cc28c07b9bb546c
   edit_date: { type: Date, default: Date.now },
   rate: {
     type: Number,
     required: false,
-    enum: [0, 1, 2, 3, 4, 5]
+    enum: [0, 1, 2, 3, 4, 5],
   },
 });
 
 const rate = new schema({
   user: {
     type: schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   hotel: {
     type: schema.Types.ObjectId,
-    ref: "Hotel"
+    ref: "Hotel",
   },
-<<<<<<< HEAD
-  service: {
-    type: schema.Types.ObjectId,
-    ref: "Service"
-  },
-=======
->>>>>>> 7292a7bb72881106dacfc1257cc28c07b9bb546c
   comment: String,
   rate: {
     type: Number,
     required: false,
-    enum: [0, 1, 2, 3, 4, 5]
+    enum: [0, 1, 2, 3, 4, 5],
   },
-
-<<<<<<< HEAD
-})
-
-
-const service = new schema({
-  name: { type: String, unique: true, required: true },
-  images: [String],
-=======
 });
 
 const booking = new schema({
   user: {
     type: schema.Types.ObjectId,
-    ref: "User"
+    ref: "User",
   },
   hotel: {
     type: schema.Types.ObjectId,
-    ref: "Hotel"
+    ref: "Hotel",
   },
   description: String,
   created_date: { type: Date, default: Date.now },
 });
-
 
 const service = new schema({
   name: { type: String, required: true },
   hotelId: {
     type: schema.Types.ObjectId,
     ref: "Hotel",
-    required: true 
+    required: true,
   },
->>>>>>> 7292a7bb72881106dacfc1257cc28c07b9bb546c
   description: String,
   edit_date: { type: Date, default: Date.now },
   rate: {
     type: Number,
     required: false,
-    enum: [0, 1, 2, 3, 4, 5]
+    enum: [0, 1, 2, 3, 4, 5],
   },
 });
 
-
-
-const User = mongoose.model('User', user);
-<<<<<<< HEAD
-const Categories = mongoose.model('Categories', categories);
-const Hotels = mongoose.model('Hotels', hotels);
-const Service = mongoose.model('Service', service);
-const Rate = mongoose.model('Rate', rate);
-
-module.exports = {
-  users: User,
-  categories: Categories,
-  hotels: Hotels,
-  service: Service,
-  rate: Rate
-=======
-const Hotels = mongoose.model('Hotels', hotels);
-const Service = mongoose.model('Service', service);
-const Rate = mongoose.model('Rate', rate);
-const Booking = mongoose.model('Booking', booking);
+const User = mongoose.model("User", user);
+const Hotels = mongoose.model("Hotels", hotels);
+const Service = mongoose.model("Service", service);
+const Rate = mongoose.model("Rate", rate);
+const Booking = mongoose.model("Booking", booking);
 
 module.exports = {
   users: User,
   hotels: Hotels,
   service: Service,
   rate: Rate,
-  booking:Booking
->>>>>>> 7292a7bb72881106dacfc1257cc28c07b9bb546c
-}
+  booking: Booking,
+};
